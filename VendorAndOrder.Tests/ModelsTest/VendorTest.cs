3639dc5350ed09f6e7650a0f8c1602cdbd835e80
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-15 01:43:50 -0700

    The vendor constructor successfully passes the test to return string

--- VendorAndOrder.Tests/ModelsTest/VendorTest.cs
+++ VendorAndOrder.Tests/ModelsTest/VendorTest.cs
@@ -5,13 +5,21 @@ using System;
 namespace VendorAndOrder.Tests
 {
   [TestClass]
-  public class Tests
+  public class VendorTests
   {
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("Vendor", " Its a bakery");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void VendorName_ReturnsName_String()
+    {
+      string vendor = "Suzie Bakery";
+      Vendor newVendor = new Vendor(vendor, "test description");
+      string result = newVendor.VendorName;
+      Assert.AreEqual(vendor, result);
     }
   }
 }
